2d434e14c0002e90ea834c5f73b4b1913d42f628
Phanumatlaloed <phanumat.la.65@ubu.ac.th>
2026-07-01 16:05:23 +0700

    add mind api

--- main.go
+++ main.go
@@ -12,5 +12,10 @@ func main() {
 		return c.SendString("นี่คือหัวหน้า")
 	})
 
+	// API ของสมาชิก mind
+	app.Get("/mind", func(c *fiber.Ctx) error {
+		return c.SendString("นี่คือสมาชิก mind")
+	})
+
 	app.Listen(":3000")
 }
